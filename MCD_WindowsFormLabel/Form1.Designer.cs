62cf619d7076fcd3f60058bb86f61e5c40d77eda
emre gemici <gmcc.emree@gmail.com>
2021-12-08 17:04:24 +0300

    Add project files.

---
+++ MCD_WindowsFormLabel/Form1.Designer.cs
@@ -0,0 +1,112 @@
+﻿
+namespace MCD_WindowsFormLabel
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblKulaniciAd = new System.Windows.Forms.Label();
+            this.lblsifre = new System.Windows.Forms.Label();
+            this.txtKullaniciAdi = new System.Windows.Forms.TextBox();
+            this.txtSifre = new System.Windows.Forms.TextBox();
+            this.btnGirisYap = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblKulaniciAd
+            // 
+            this.lblKulaniciAd.AutoSize = true;
+            this.lblKulaniciAd.Font = new System.Drawing.Font("Tahoma", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblKulaniciAd.Location = new System.Drawing.Point(281, 107);
+            this.lblKulaniciAd.Name = "lblKulaniciAd";
+            this.lblKulaniciAd.Size = new System.Drawing.Size(122, 21);
+            this.lblKulaniciAd.TabIndex = 0;
+            this.lblKulaniciAd.Text = "Kullanıcı Adı:";
+            // 
+            // lblsifre
+            // 
+            this.lblsifre.AutoSize = true;
+            this.lblsifre.Font = new System.Drawing.Font("Tahoma", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblsifre.Location = new System.Drawing.Point(281, 138);
+            this.lblsifre.Name = "lblsifre";
+            this.lblsifre.Size = new System.Drawing.Size(56, 21);
+            this.lblsifre.TabIndex = 1;
+            this.lblsifre.Text = "Şifre:";
+            // 
+            // txtKullaniciAdi
+            // 
+            this.txtKullaniciAdi.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtKullaniciAdi.Location = new System.Drawing.Point(409, 106);
+            this.txtKullaniciAdi.Name = "txtKullaniciAdi";
+            this.txtKullaniciAdi.Size = new System.Drawing.Size(138, 26);
+            this.txtKullaniciAdi.TabIndex = 2;
+            // 
+            // txtSifre
+            // 
+            this.txtSifre.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtSifre.Location = new System.Drawing.Point(409, 138);
+            this.txtSifre.Name = "txtSifre";
+            this.txtSifre.PasswordChar = '*';
+            this.txtSifre.Size = new System.Drawing.Size(138, 26);
+            this.txtSifre.TabIndex = 3;
+            // 
+            // btnGirisYap
+            // 
+            this.btnGirisYap.Font = new System.Drawing.Font("Tahoma", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnGirisYap.Location = new System.Drawing.Point(433, 188);
+            this.btnGirisYap.Name = "btnGirisYap";
+            this.btnGirisYap.Size = new System.Drawing.Size(75, 23);
+            this.btnGirisYap.TabIndex = 4;
+            this.btnGirisYap.Text = "Giriş Yap";
+            this.btnGirisYap.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(942, 569);
+            this.Controls.Add(this.btnGirisYap);
+            this.Controls.Add(this.txtSifre);
+            this.Controls.Add(this.txtKullaniciAdi);
+            this.Controls.Add(this.lblsifre);
+            this.Controls.Add(this.lblKulaniciAd);
+            this.Name = "Form1";
+            this.Text = "Giriş Ekranı";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblKulaniciAd;
+        private System.Windows.Forms.Label lblsifre;
+        private System.Windows.Forms.TextBox txtKullaniciAdi;
+        private System.Windows.Forms.TextBox txtSifre;
+        private System.Windows.Forms.Button btnGirisYap;
+    }
+}
+
---
+++ MCD_WindowsFormLabel/sanalDatabase.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MCD_WindowsFormLabel
+{
+    public class sanalDatabase
+    {
+        public static List<kullanici> KullaniciTablo = new List<kullanici>();
+        static sanalDatabase()
+        {
+            KullaniciTablo.Add(new kullanici()
+            {
+                id = 1,
+                isim="Emre",
+                soyisim ="Gemici",
+                kullaniciAdi ="emre.gemici",
+                sifre ="1",
+                aciklama ="Emre Gemici Kullanıcı Profil Bilgisi"
+            });
+
+            KullaniciTablo.Add(new kullanici()
+            {
+                id = 2,
+                isim = "Elif",
+                soyisim = "Gemici",
+                kullaniciAdi = "elif.gemici",
+                sifre = "2",
+                aciklama = "Elif Gemici Kullanıcı Profil Bilgisi"
+            });
+        }
+    }
+}
